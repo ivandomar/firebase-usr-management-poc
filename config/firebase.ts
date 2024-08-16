ca69d339d2645d788739114680c473ea41504c70
Ivan Cézanne Seara <ivancezanne@gmail.com>
2024-08-16 01:55:41 -0300

    fixes firebase project id config identifier

--- config/firebase.ts
+++ config/firebase.ts
@@ -2,13 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 import loader from './utils/loader';
+import { exit } from 'process';
 
 loader.load();
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_APROJECT_ID,
+  projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID
