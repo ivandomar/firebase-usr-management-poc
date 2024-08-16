1cc40aa2a56ec9b85cce64c55589fbe28ef8e916
Ivan Cézanne Seara <ivancezanne@gmail.com>
2024-08-16 02:40:58 -0300

    implements signup business logic

--- src/controllers/Sign.ts
+++ src/controllers/Sign.ts
@@ -1,10 +1,24 @@
 import { Response } from 'express';
 
 import { Signup } from "./requests/Signup";
+import AuthService from '../services/Auth';
+import DataStorageService from '../services/DataStorage';
+import { error } from 'console';
 
 const SignController = {
-    signup: (req: Signup, res: Response) => {
-        res.json(req.email + '--' + req.password);
+    signup: async (req: Signup, res: Response) => {
+        try {
+            const { user } = await AuthService.signup(req.email, req.password);
+
+            if (!user) {
+                throw Error('User has not defined an email');
+            } else {
+                await DataStorageService.store('Users',  { email: req.email });
+            }
+
+        } catch (error) {
+            console.error(error);
+        }
     },
 };
 
